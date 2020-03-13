b558fb15c086d385c153e4c01bb630c17960068b
Neal Fultz <nfultz@gmail.com>
2020-03-13 00:55:30 +0000

    adding crude test

---
+++ tests/barplot.R
@@ -0,0 +1,12 @@
+
+osc1337:::nullpng()
+
+barplot(1:10)
+
+z <- capture.output(osc1337::osc1337())
+
+z <- substring(z[4], 900, 999)
+z <- sum(as.integer(charToRaw(z)))
+
+stopifnot(identical(z, 8545L)) # Awful hash
+
